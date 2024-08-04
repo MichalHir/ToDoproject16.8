093ba5bbeb0711399049d6d6c04a121ef639f6d8
MichalHir <michalaizi@gmail.com>
2024-08-04 21:22:30 +0300

    added login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request
 
 # from jinja2 import Template
 
@@ -10,6 +10,17 @@ from data import client, to_do
 @app.route("/")
 def main_page():
     return render_template('main_page.html', client=client)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        list=[]
+    flash('Login successful!', 'success')
+
+    return redirect('/')
+return render_template('login.html')
 
 
 if __name__ == "__main__":
